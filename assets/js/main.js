af3b5dcfcd7eba6700a2285e71ca8425053a448f
Adam Strzelczyk <strzelczyk337@wp.pl>
2022-11-11 16:07:38 +0100

    assets

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+(function() {
+    
+    "use strict";
+    
+    //===== Prealoder
+
+    window.onload = function() {
+        window.setTimeout(fadeout, 500);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+    $('.navTrigger').click(function () {
+        $(this).toggleClass('active');
+        console.log("Clicked menu");
+        $("#mainListDiv").toggleClass("show_list");
+        $("#mainListDiv").fadeIn();
+    
+    });
+    
+
+    
+})();
